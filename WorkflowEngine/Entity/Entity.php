51914bb7fe3751979313b632828719ea781cdeed
David Molineus <mail@netzmacht.de>
2013-10-29 10:30:30 +0100

    implement ModelState based on DcGeneral Model interface

---
+++ WorkflowEngine/Entity/Entity.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace WorkflowEngine\Entity;
+
+use DcGeneral\Data\ModelInterface;
+
+/**
+ * Class Entity is a wrapper for models using DcGeneral\Data\ModelInterface. The wrapper is required so that the
+ * WorkflowEngine will stay independent on the used model class
+ *
+ * @package WorkflowEngine\Entity
+ */
+class Entity implements ModelInterface
+{
+
+	/**
+	 * @var \DcGeneral\Data\ModelInterface
+	 */
+	protected $model;
+
+
+	/**
+	 * @param ModelInterface $model
+	 */
+	public function __construct(ModelInterface $model)
+	{
+		$this->model = $model;
+	}
+
+
+	/**
+	 * Get the id for this model.
+	 *
+	 * @return mixed The Id for this model.
+	 */
+	public function getId()
+	{
+		return $this->model->getId();
+	}
+
+
+	/**
+	 * (PHP 5 &gt;= 5.0.0)<br/>
+	 * Retrieve an external iterator
+	 * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
+	 * @return \Traversable An instance of an object implementing <b>Iterator</b> or
+	 * <b>Traversable</b>
+	 */
+	public function getIterator()
+	{
+		return $this->model->getIterator();
+	}
+
+
+	/**
+	 * Fetch the property with the given name from the model.
+	 *
+	 * This method returns null if an unknown property is retrieved.
+	 *
+	 * @param string $strPropertyName The property name to be retrieved.
+	 *
+	 * @return mixed The value of the given property.
+	 */
+	public function getProperty($strPropertyName)
+	{
+		return $this->model->getProperty($strPropertyName);
+	}
+
+
+	/**
+	 * Fetch all properties from the model as an name => value array.
+	 *
+	 * @return array
+	 */
+	public function getPropertiesAsArray()
+	{
+		return $this->model->getPropertiesAsArray();
+	}
+
+
+	/**
+	 * Fetch meta information from model.
+	 *
+	 * @param string $strMetaName The meta information to retrieve.
+	 *
+	 * @return mixed The set meta information or null if undefined.
+	 */
+	public function getMeta($strMetaName)
+	{
+		return $this->model->getMeta($strMetaName);
+	}
+
+
+	/**
+	 * Set the id for this object.
+	 *
+	 * NOTE: when the Id has been set once to a non null value, it can NOT be changed anymore.
+	 *
+	 * Normally this should only be called from inside of the implementing provider.
+	 *
+	 * @param mixed $mixId Could be a integer, string or anything else - depends on the provider implementation.
+	 *
+	 * @return void
+	 */
+	public function setId($mixId)
+	{
+		$this->setId($mixId);
+	}
+
+
+	/**
+	 * Update the property value in the model.
+	 *
+	 * @param string $strPropertyName
+	 *
+	 * @param mixed $varValue
+	 *
+	 * @return void
+	 */
+	public function setProperty($strPropertyName, $varValue)
+	{
+		$this->setProperty($strPropertyName, $varValue);
+	}
+
+
+	/**
+	 * Update all properties in the model.
+	 *
+	 * @param array $arrProperties The property values as name => value pairs.
+	 *
+	 * @return void
+	 */
+	public function setPropertiesAsArray($arrProperties)
+	{
+		$this->setPropertiesAsArray($arrProperties);
+	}
+
+
+	/**
+	 * Update meta information in the model.
+	 *
+	 * @param string $strMetaName The meta information name.
+	 *
+	 * @param mixed $varValue The meta information value to store.
+	 *
+	 * @return void
+	 */
+	public function setMeta($strMetaName, $varValue)
+	{
+		$this->setMeta($strMetaName, $varValue);
+	}
+
+
+	/**
+	 * Check if this model have any properties.
+	 *
+	 * @return boolean true if any property has been stored, false otherwise.
+	 */
+	public function hasProperties()
+	{
+		return $this->model->hasProperties();
+	}
+
+
+	/**
+	 * Return the data provider name.
+	 *
+	 * @return string the name of the corresponding data provider.
+	 */
+	public function getProviderName()
+	{
+		return $this->model->getProviderName();
+	}
+
+
+	/**
+	 * Copy this model, without the id.
+	 *
+	 * @return \DcGeneral\Data\ModelInterface
+	 */
+	public function __clone()
+	{
+		$this->model = clone $this->model;
+	}
+
+} 
--- WorkflowEngine/Entity/ModelState.php
+++ WorkflowEngine/Entity/ModelState.php
@@ -2,270 +2,231 @@
 
 namespace WorkflowEngine\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
+use DcGeneral\Data\ModelInterface;
 
-/**
- * Used to store a state of a model object.
- *
- */
-class ModelState
+
+class ModelState extends Entity
 {
-    /**
-     * @var int
-     */
-    protected $id;
+	/**
+	 * @var int
+	 */
+	protected $id;
 
-    /**
-     * @var string
-     */
-    protected $workflowIdentifier;
 
-    /**
-     * @var string
-     */
-    protected $processName;
+	/**
+	 * Construct.
+	 */
+	public function __construct(ModelInterface $model)
+	{
+		$this->model = $model;
+		$this->setProperty('createdAt', time());
+		$this->setProperty('next', array());
+	}
 
-    /**
-     * @var string
-     */
-    protected $stepName;
 
-    /**
-     * @var boolean
-     */
-    protected $successful;
+	/**
+	 * Get workflowIdentifier
+	 *
+	 * @return string
+	 */
+	public function getWorkflowIdentifier()
+	{
+		return $this->getProperty('workflowIdentifier');
+	}
 
-    /**
-     * @var \DateTime
-     */
-    protected $createdAt;
+	/**
+	 * Set workflowIdentifier
+	 *
+	 * @param string $workflowIdentifier
+	 */
+	public function setWorkflowIdentifier($workflowIdentifier)
+	{
+		$this->setProperty('workflowIdentifier', $workflowIdentifier);
+	}
 
-    /**
-     * @var array
-     */
-    protected $data;
+	/**
+	 * Get processName
+	 *
+	 * @return string
+	 */
+	public function getProcessName()
+	{
+		return $this->getProperty('processName');
+	}
 
-    /**
-     * @var array
-     */
-    protected $errors;
 
-    /**
-     * @var ModelState
-     */
-    protected $previous;
+	/**
+	 * Set processName
+	 *
+	 * @param string $processName
+	 */
+	public function setProcessName($processName)
+	{
+		$this->setProperty('processName', $processName);
+	}
 
-    /**
-     * @var ArrayCollection
-     */
-    protected $next;
 
-    /**
-     * Construct.
-     */
-    public function __construct()
-    {
-        $this->createdAt = new \DateTime('now');
-        $this->next = new ArrayCollection();
-    }
+	/**
+	 * Get stepName
+	 *
+	 * @return string
+	 */
+	public function getStepName()
+	{
+		return $this->getProperty('stepName');
+	}
 
-    /**
-     * Get Id
-     *
-     * @return int
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
 
-    /**
-     * Get workflowIdentifier
-     *
-     * @return string
-     */
-    public function getWorkflowIdentifier()
-    {
-        return $this->workflowIdentifier;
-    }
+	/**
+	 * Set stepName
+	 *
+	 * @param string $stepName
+	 */
+	public function setStepName($stepName)
+	{
+		$this->setProperty('stepName', $stepName);
+	}
 
-    /**
-     * Set workflowIdentifier
-     *
-     * @param string $workflowIdentifier
-     */
-    public function setWorkflowIdentifier($workflowIdentifier)
-    {
-        $this->workflowIdentifier = $workflowIdentifier;
-    }
 
-    /**
-     * Get processName
-     *
-     * @return string
-     */
-    public function getProcessName()
-    {
-        return $this->processName;
-    }
+	/**
+	 * Get createdAt
+	 *
+	 * @return int timestamp
+	 */
+	public function getCreatedAt()
+	{
+		return $this->getProperty('createdAt');
+	}
 
-    /**
-     * Set processName
-     *
-     * @param string $processName
-     */
-    public function setProcessName($processName)
-    {
-        $this->processName = $processName;
-    }
+	/**
+	 * Set createdAt
+	 *
+	 * @param int $createdAt timestamp
+	 */
+	public function setCreatedAt($createdAt)
+	{
+		$this->setProperty('createdAt', $createdAt);
+	}
 
-    /**
-     * Get stepName
-     *
-     * @return string
-     */
-    public function getStepName()
-    {
-        return $this->stepName;
-    }
 
-    /**
-     * Set stepName
-     *
-     * @param string $stepName
-     */
-    public function setStepName($stepName)
-    {
-        $this->stepName = $stepName;
-    }
+	/**
+	 * Get data
+	 *
+	 * @return array
+	 */
+	public function getData()
+	{
+		return deserialize($this->getProperty('data'), true);
+	}
 
-    /**
-     * Get createdAt
-     *
-     * @return \DateTime
-     */
-    public function getCreatedAt()
-    {
-        return $this->createdAt;
-    }
 
-    /**
-     * Set createdAt
-     *
-     * @param DateTime $createdAt
-     */
-    public function setCreatedAt($createdAt)
-    {
-        $this->createdAt = $createdAt;
-    }
+	/**
+	 * Set data
+	 *
+	 * @param mixed $data An array or a JSON string
+	 */
+	public function setData($data)
+	{
+		if (!is_string($data)) {
+			$data = serialize($data);
+		}
 
-    /**
-     * Get data
-     *
-     * @return array
-     */
-    public function getData()
-    {
-        return json_decode($this->data, true);
-    }
+		$this->setProperty('data', $data);
+	}
 
-    /**
-     * Set data
-     *
-     * @param mixed $data An array or a JSON string
-     */
-    public function setData($data)
-    {
-        if (!is_string($data)) {
-            $data = json_encode($data);
-        }
 
-        $this->data = $data;
-    }
+	/**
+	 * Get successful
+	 *
+	 * @return boolean
+	 */
+	public function getSuccessful()
+	{
+		return (boolean) $this->getProperty('successful');
+	}
 
-    /**
-     * Get successful
-     *
-     * @return boolean
-     */
-    public function getSuccessful()
-    {
-        return $this->successful;
-    }
 
-    /**
-     * Set successful
-     *
-     * @param boolean
-     */
-    public function setSuccessful($successful)
-    {
-        $this->successful = (boolean) $successful;
-    }
+	/**
+	 * Set successful
+	 *
+	 * @param boolean
+	 */
+	public function setSuccessful($successful)
+	{
+		$this->setProperty('successful', (boolean) $successful);
+	}
 
-    /**
-     * Get errors
-     *
-     * @return string
-     */
-    public function getErrors()
-    {
-        return json_decode($this->errors, true);
-    }
 
-    /**
-     * Set errors
-     *
-     * @param string $errors
-     */
-    public function setErrors($errors)
-    {
-        if (!is_string($errors)) {
-            $errors = json_encode($errors);
-        }
+	/**
+	 * Get errors
+	 *
+	 * @return array
+	 */
+	public function getErrors()
+	{
+		return deserialize($this->getProperty('errors'), true);
+	}
 
-        $this->errors = $errors;
-    }
 
-    /**
-     * Get previous
-     *
-     * @return \WorkflowEngine\Entity\ModelState
-     */
-    public function getPrevious()
-    {
-        return $this->previous;
-    }
+	/**
+	 * Set errors
+	 *
+	 * @param string $errors
+	 */
+	public function setErrors($errors)
+	{
+		if (!is_string($errors)) {
+			$errors = json_encode($errors);
+		}
 
-    /**
-     * Set previous
-     *
-     * @param ModelState $state
-     */
-    public function setPrevious(ModelState $state)
-    {
-        $this->previous = $state;
-    }
+		$this->setProperty('errors', $errors);
+	}
 
-    /**
-    * Get next
-    *
-    * @return ArrayCollection
-    */
-    public function getNext()
-    {
-        return $this->next;
-    }
 
-    /**
-     * Add next
-     *
-     * @param ModelState $state
-     */
-    public function addNext(ModelState $state)
-    {
-        $state->setPrevious($this);
+	/**
+	 * Get previous
+	 *
+	 * @return \WorkflowEngine\Entity\ModelState
+	 */
+	public function getPrevious()
+	{
+		return $this->getProperty('previous');
+	}
 
-        $this->next[] = $state;
-    }
+
+	/**
+	 * Set previous
+	 *
+	 * @param ModelState $state
+	 */
+	public function setPrevious(ModelState $state)
+	{
+		$this->setProperty('previous', $state->getId());
+	}
+
+
+	/**
+	 * Get next
+	 *
+	 * @return array
+	 */
+	public function getNext()
+	{
+		return $this->getProperty('next');
+	}
+
+
+	/**
+	 * Add next
+	 *
+	 * @param ModelState $state
+	 */
+	public function addNext(ModelState $state)
+	{
+		$state->setPrevious($this);
+
+		$next = $this->getProperty('next');
+		$next[] = $state;
+	}
+
 }
