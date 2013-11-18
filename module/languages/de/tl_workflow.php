cde2516d210a1919857a33c91f3a03fde8826279
David Molineus <mail@netzmacht.de>
2013-11-18 19:49:35 +0100

    updated dca files

--- module/languages/de/tl_workflow.php
+++ module/languages/de/tl_workflow.php
@@ -3,9 +3,8 @@
 /**
  * legends
  */
-$GLOBALS['TL_LANG']['tl_workflow']['title_legend'] = 'Workflow';
-$GLOBALS['TL_LANG']['tl_workflow']['scope_legend'] = 'Geltungsbereich';
-$GLOBALS['TL_LANG']['tl_workflow']['services_legend'] = 'Services';
+$GLOBALS['TL_LANG']['tl_workflow']['workflow_legend']   = 'Workflow';
+$GLOBALS['TL_LANG']['tl_workflow']['processes_legend']   = 'Tabellen & Prozesse';
 
 
 /**
@@ -14,26 +13,27 @@ $GLOBALS['TL_LANG']['tl_workflow']['services_legend'] = 'Services';
 $GLOBALS['TL_LANG']['tl_workflow']['btn_services'][0] = 'Services';
 $GLOBALS['TL_LANG']['tl_workflow']['btn_services'][1] = 'Services anlegen und verwalten';
 
+$GLOBALS['TL_LANG']['tl_workflow']['new'][0] = 'Workflow anlegen';
+$GLOBALS['TL_LANG']['tl_workflow']['new'][1] = 'Einen neuen Workflow anlegen';
+
+$GLOBALS['TL_LANG']['tl_workflow']['btn_process'][0] = 'Prozesse konfigurieren';
+$GLOBALS['TL_LANG']['tl_workflow']['btn_process'][1] = 'Workflow-Prozesse anlegen und konfigurieren';
+
+
 /**
  * fields
  */
 $GLOBALS['TL_LANG']['tl_workflow']['title'][0] = 'Titel';
 $GLOBALS['TL_LANG']['tl_workflow']['title'][1] = 'Geben Sie einen Titel des Workflows an.';
 
+$GLOBALS['TL_LANG']['tl_workflow']['processes'][0] = 'Prozesse zuweisen';
+$GLOBALS['TL_LANG']['tl_workflow']['processes'][1] = 'Weisen Sie den unterstützten Tabellen einen Worfklow-Prozess zu.';
+
+$GLOBALS['TL_LANG']['tl_workflow']['table'][0] = 'Tabelle';
+$GLOBALS['TL_LANG']['tl_workflow']['table'][1] = 'Wählen Sie eine Tabelle aus.';
+
 $GLOBALS['TL_LANG']['tl_workflow']['process'][0] = 'Prozess';
-$GLOBALS['TL_LANG']['tl_workflow']['process'][1] = 'Wählen Sie einen Prozess aus, der verwendet werden soll.';
+$GLOBALS['TL_LANG']['tl_workflow']['process'][1] = 'Wählen Sie einen Prozess aus, der für die Tabelle verwendet werden soll.';
 
-$GLOBALS['TL_LANG']['tl_workflow']['forModule'][0] = 'Modul beschränken';
-$GLOBALS['TL_LANG']['tl_workflow']['forModule'][1] = 'Sie können die Gültigkeit des Workflows auf ein bestimmtes Modul beschränken.';
-
-$GLOBALS['TL_LANG']['tl_workflow']['forTable'][0] = 'Tabelle';
-$GLOBALS['TL_LANG']['tl_workflow']['forTable'][1] = 'Wählen Sie die Tabelle für den Workflow aus.';
-
-$GLOBALS['TL_LANG']['tl_workflow']['services'][0] = 'Services';
-$GLOBALS['TL_LANG']['tl_workflow']['services'][1] = 'Weißen Sie dem Workflow Service zu, die ausgeführt werden sollen.';
-
-$GLOBALS['TL_LANG']['tl_workflow']['service'][0] = 'Service';
-$GLOBALS['TL_LANG']['tl_workflow']['service'][1] = 'Wählen Sie einen Servic aus.';
-
-$GLOBALS['TL_LANG']['tl_workflow']['service_disabled'][0] = 'Deaktivieren';
-$GLOBALS['TL_LANG']['tl_workflow']['service_disabled'][1] = 'Sie können einen Service vorübergehend deaktivieren ohne ihn aus der Liste zu entfernen.';
+$GLOBALS['TL_LANG']['tl_workflow']['workflow'][0] = 'Typ auswählen';
+$GLOBALS['TL_LANG']['tl_workflow']['workflow'][1] = 'Wählen Sie einen installierten Workflow-Typ aus.';
